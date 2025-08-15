42aa546448fc299579bc4da631a4827f6d1ce3c0
thiagosoethe <thiago_paschoal@estudante.sc.senai.br>
2025-08-14 21:50:09 -0300

    desafio 3 vetor

--- javascript/vetorunidimencional/desafio 3/js/validaproc.js
+++ javascript/vetorunidimencional/desafio 3/js/validaproc.js
@@ -1,27 +1,13 @@
 function validaProc() {
+    let vetor = [];
 
-    let num = [];
-    for (let i = 0; i < 3; i++) {
-        num[i] = parseFloat(prompt("Digite a nota " + (i + 1) + ":"));
-        num
+    for (let i = 0; i < 10; i++) {
+        vetor[i] = i * i;
+    }
 
-
-        console.log("todas notas");
-
-        for (let i = 0; i < 3; i++) {
-            console.log("valor de i: " + i);
-        }
-        console.log("notas maiores ou iguais a 7,5");
-
-        for (i = 0; i < 3; i++) {
-            if (num[i] >= 7.5) {
-                console.log("nota maior ou igual a 7,5: " + num[i]);
-            }
-            if (num[i] < 7.5) {
-                console.log("nota menor que 7,5: " + num[i]);
-            }
-        }
-        return false;
-
+    console.log("=== VETOR DE QUADRADOS ===");
+    for (let i = 0; i < vetor.length; i++) {
+        console.log("Posição " + i + " : " + vetor[i]);
     }
 }
+
